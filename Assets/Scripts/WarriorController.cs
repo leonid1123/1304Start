039a4078ca1568865e23360cc20df091404515e9
Leo <47742726+leonid1123@users.noreply.github.com>
2022-02-28 12:41:49 +0300

    minor fix

--- Assets/Scripts/WarriorController.cs
+++ Assets/Scripts/WarriorController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WarriorController : MonoBehaviour
 {
     public Rigidbody2D rb2d; //для работы с физ телом
-    private float speed = 500f; //для изменения скорости
+    private float speed = 600f; //для изменения скорости
     private bool toRight = true; //для отслеживания куда идём
 
     public groundCheck groundCheck;
@@ -17,7 +17,7 @@ public class WarriorController : MonoBehaviour
     void Update()
     {
         //меняем скорость
-        rb2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, rb2d.velocity.y);
+        rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, rb2d.velocity.y);
         if(toRight && rb2d.velocity.x<0)//если идем направо, но скорость отрицательная
         {
             toRight = false; //направо не идем
@@ -30,7 +30,7 @@ public class WarriorController : MonoBehaviour
         }
         if (Input.GetButton("Jump") && groundCheck.isGround())
         {
-            rb2d.velocity = Vector2.up*5f;
+            rb2d.velocity = Vector2.up*8f;
         }
     }
 
